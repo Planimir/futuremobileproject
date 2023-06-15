50b0934935a7a284550b170805f0ab6d3ddcc5e4
Planimir Yordanov <planimir@me.com>
2023-06-15 15:02:57 +0300

    Commiting the whole project at once.

---
+++ app/src/main/java/com/example/databaseformwithdynamiclinks/Act_Log_or_Reg.java
@@ -0,0 +1,14 @@
+package com.example.databaseformwithdynamiclinks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Act_Log_or_Reg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_log_or_reg);
+    }
+}
